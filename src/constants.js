380ef0564e00b56730a47e433f6a212d494f32cd
Spl3en <spl3en.contact@gmail.com>
2020-03-04 23:18:54 +0100

    Update Yeouido contract address

--- src/constants.js
+++ src/constants.js
@@ -8,10 +8,10 @@ export const Networks = Object.freeze({
 export const WALLET_LOCAL_STORAGE_KEY = 'wallet'
 
 // SCORE Configuration
-// export const SCORE_ENDPOINT = 'cx73d22bf8608407339b8531f6c79e391baf7a6225'
-// export const SCORE_NETWORK = Networks.YEOUIDO
-export const SCORE_ENDPOINT = "cx9c08deb7f3f6bbc4a66e9b34d04c1d05a54708d3"
-export const SCORE_NETWORK = Networks.LOCALHOST
+export const SCORE_ENDPOINT = 'cx66ab44d7f1a87ea86847c40629586ff0837238f6'
+export const SCORE_NETWORK = Networks.YEOUIDO
+// export const SCORE_ENDPOINT = "cx9c08deb7f3f6bbc4a66e9b34d04c1d05a54708d3"
+// export const SCORE_NETWORK = Networks.LOCALHOST
 
 // ICX Configuration
 export const ICX_TOKEN_CONTRACT = 'cx0000000000000000000000000000000000000000'
